178ac5927b501ce46814a97c794c8b25338cb656
Matthias Osswald <m@osswald.li>
2019-06-19 10:24:38 +0200

    Implement multiprocessing logger

--- multiprocessing_logging/console.py
+++ multiprocessing_logging/console.py
@@ -1,20 +1,69 @@
-from multiprocessing import Pool
-from time import sleep
-import datetime
+from datetime import datetime
+import logging
+import multiprocessing
+import os
+import sys
+
+# python version dependent import
+if (sys.version_info > (3, 0)):
+    from logging.handlers import QueueHandler, QueueListener
+else:
+    from logutils import QueueHandler, QueueListener
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+log_path = os.path.join(current_dir, 'log_example.log')
+logging.basicConfig(filename=log_path)
+# test_logger = logging.getLogger('test_logger')
 
 
-ELEMENTS = [True for v in range(1000)]
+def do_job(i):
+    # time.sleep(random())
+    logging.info('do_job called with: {} => {}'.format(i, datetime.now()))
+    return i*2
 
 
-def repeat_for_each_element(element):
-    print(datetime.datetime.now())
-    sleep(1)
+def worker_init(q):
+    # all records from worker processes go to qh and then into q
+    qh = QueueHandler(q)
+    logger = logging.getLogger('test_logger')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(qh)
+
+
+def logger_init():
+    q = multiprocessing.Queue()
+    # this is the handler for all log records
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(levelname)s: %(asctime)s - %(process)s - %(message)s"))
+
+    # ql gets records from the queue and sends them to the handler
+    ql = QueueListener(q, handler)
+    ql.start()
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    # add the handler to the logger so records from this process are handled
+    logger.addHandler(handler)
+
+    return ql, q
 
 
 def main():
-    pool = Pool(processes=100)
-    pool.map(repeat_for_each_element, ELEMENTS)
+    tasks = [x for x in range(100)]
+    processes = 100
+    q_listener, q = logger_init()
+
+    logging.info('Hi, I\'m the main thread')
+
+    pool = multiprocessing.Pool(processes, worker_init, [q])
+    return_values = pool.map(do_job, tasks)
+
+    print(return_values)
+
+    pool.close()
+    pool.join()
+    q_listener.stop()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
