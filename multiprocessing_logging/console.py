131d04a61489a56b19ca04b2a6d2a7cbf7ce4d5b
Matthias Osswald <m@osswald.li>
2019-08-05 15:32:01 +0200

    Adapt naming and use random time to clarify the process

--- multiprocessing_logging/console.py
+++ multiprocessing_logging/console.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from random import randrange
 import logging
 import multiprocessing
 import os
 import sys
+import time
 
 # python version dependent import
 # python < 3.2 does not provide methods in std. lib
@@ -14,13 +16,13 @@ else:
 current_dir = os.path.dirname(os.path.abspath(__file__))
 log_path = os.path.join(current_dir, 'log_example.log')
 logging.basicConfig(filename=log_path)
-# test_logger = logging.getLogger('test_logger')
 
 
-def do_job(i):
-    # time.sleep(random())
-    logging.info('do_job called with: {} => {}'.format(i, datetime.now()))
-    return i*2
+def do_job(task):
+    random_int = randrange(10)
+    time.sleep(random_int)
+    logging.info('do_job called with: {} => {}'.format(task, datetime.now()))
+    return random_int
 
 
 def worker_init(q):
@@ -51,20 +53,28 @@ def logger_init():
 
 def main():
     tasks = [x for x in range(100)]
+    # if there is no danger that the system can't handle the processes
+    # it seems to be best to assign as many processes as there are tasks
+    # to get the maximum speed for I/O tasks.
     processes = 100
     q_listener, q = logger_init()
 
-    logging.info('Hi, I\'m the main thread')
+    test_logger = logging.getLogger('test_logger')
+    test_logger.info('Hi, I\'m the main thread')
 
     pool = multiprocessing.Pool(processes, worker_init, [q])
-    return_values = pool.map(do_job, tasks)
 
-    print(return_values)
+    start_time = time.time()
+    return_values = pool.map(do_job, tasks)
+    end_time = time.time()
 
     pool.close()
     pool.join()
     q_listener.stop()
 
+    test_logger.info('Hi, from the main thread again')
+    print('*** Handling all tasks took {} seconds ***'.format(
+        end_time - start_time))
 
 if __name__ == '__main__':
     main()
